93cc303efc32551055b4d740f5a01818ea64d755
tamellal <36163467+tamellal@users.noreply.github.com>
2021-02-12 15:18:11 +0000

    Deploying to gh-pages from master @ 6797f13b77142f358411d87f038683497ac2ad05 🚀

---
+++ isp-v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="isp-v2" tilewidth="32" tileheight="32" tilecount="33" columns="11">
+ <image source="../../../../../Documents/isp-v2.png" width="372" height="110"/>
+</tileset>
