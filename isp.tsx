6797f13b77142f358411d87f038683497ac2ad05
Toufik <t.amellal@thecodingmachine.com>
2021-02-12 16:17:30 +0100

    Initial commit

---
+++ isp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="isp-v3" tilewidth="32" tileheight="32" tilecount="30" columns="5">
+ <image source="../../../../../Documents/isp-v3.png" width="168" height="199"/>
+</tileset>
---
+++ office-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Office_interiors_32x32" tilewidth="32" tileheight="32" tilecount="528" columns="22">
+ <image source="../tech4good-summit-2021/tilesets/Office_interiors_32x32.png" width="704" height="768"/>
+</tileset>
